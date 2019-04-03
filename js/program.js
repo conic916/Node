d3f3578c6b8a719de122e357a810d5fa9967b941
Katarzyna Da Silva <conic916@gmail.com>
2019-04-03 20:29:28 +0200

    add new file js

--- js/program.js
+++ js/program.js
@@ -1,3 +1,4 @@
+'use strict';
 //Poprawne enkodowanie zapewnia, że program "zrozumie" co do niego mówimy (odczyta wartość jako string z kodowaniem UTF-8) :
 
 process.stdin.setEncoding('utf-8');
@@ -6,7 +7,7 @@ process.stdin.setEncoding('utf-8');
 //na zdarzenie (.on) odczytu (readable) wykonujemy funkcje :
 
 process.stdin.on('readable', function() {
-    
+
     //proste echo, czyli wyświetlamy to co wpisaliśmy w aplikację :
 
     var input = process.stdin.read();
