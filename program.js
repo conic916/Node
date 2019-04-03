fc00bb26a477a225a63a4a0a12bf53774f7f03a0
Katarzyna Da Silva <conic916@gmail.com>
2019-04-03 20:27:46 +0200

    add init project

---
+++ program.js
@@ -0,0 +1,14 @@
+//Poprawne enkodowanie zapewnia, że program "zrozumie" co do niego mówimy (odczyta wartość jako string z kodowaniem UTF-8) :
+
+process.stdin.setEncoding('utf-8');
+
+//nasłuchiwanie na zdarzenia odczytu :
+//na zdarzenie (.on) odczytu (readable) wykonujemy funkcje :
+
+process.stdin.on('readable', function() {
+    
+    //proste echo, czyli wyświetlamy to co wpisaliśmy w aplikację :
+
+    var input = process.stdin.read();
+    process.stdout.write(input)
+});
