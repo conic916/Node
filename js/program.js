ea6b1f07689b0878b9559b87fa8f2a55e8ea1e79
Katarzyna Da Silva <conic916@gmail.com>
2019-04-03 23:56:43 +0200

    add file js

--- js/program.js
+++ js/program.js
@@ -1,4 +1,4 @@
-'use strict';
+
 //Poprawne enkodowanie zapewnia, że program "zrozumie" co do niego mówimy (odczyta wartość jako string z kodowaniem UTF-8) :
 
 process.stdin.setEncoding('utf-8');
@@ -6,10 +6,18 @@ process.stdin.setEncoding('utf-8');
 //nasłuchiwanie na zdarzenia odczytu :
 //na zdarzenie (.on) odczytu (readable) wykonujemy funkcje :
 
-process.stdin.on('readable', function () {
-
-    //proste echo, czyli wyświetlamy to co wpisaliśmy w aplikację :
-
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
-    process.stdout.write(input)
+    var instruction = input.toString().trim();
+    switch (input !== null) {
+        case (instruction === '/exit') :
+            process.stdout.write('Quitting app!\n');
+            process.exit();
+        default :
+            process.stderr.write('Wrong instruction!');
+        }
 });
+
+console.log('version node.js:' + process.versions.node);
+console.log('Language node is:' + process.env.lang);
